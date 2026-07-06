b76a2d9e8c8f97c2aee8e4b608f03cf409a19cb3
Kaito321 <rars213@gmail.com>
2026-07-05 22:10:12 -0500

    Portfolio : Removed Todo, no longer in use

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -26,22 +26,6 @@ const PaperAirplane = () => (
     </svg>
 )
 
-/* --TODO: evaluate whether this will be used in the final version
-const Hammer = () => (
-    <svg
-        width="25"
-        height="25"
-        viewBox="-1.5 -1.5 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-        style={{ transform: 'scaleX(-1)' }}
-    >
-        <path
-            fill="currentColor"
-            d="m2.536 8.9l2.828 2.828l6.364-6.364l-2.829-2.828zm12.727 5.656l-4.95-4.95l-.706.708l4.95 4.95zm1.415 1.415l-.707.707l1.767 1.767a.5.5 0 0 0 .707-.707zM6.01 2.596L7.485 1.12a2 2 0 0 1 2.829 0l2.828 2.829a2 2 0 0 1 0 2.828l-1.414 1.414l8.132 8.132a2.5 2.5 0 1 1-3.536 3.536l-8.132-8.132l-1.414 1.414a2 2 0 0 1-2.828 0l-2.83-2.827a2 2 0 0 1 0-2.829l1.475-1.474a1.5 1.5 0 0 1 .293-1.708L4.303 2.89a1.5 1.5 0 0 1 1.708-.293z"
-        />
-    </svg>
-)*/
-
 const MailIcon = () => (
     <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <rect x="2" y="4" width="20" height="16" rx="2" stroke="currentColor" strokeWidth="2" />
